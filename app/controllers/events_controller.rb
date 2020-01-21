4c31e0f32bb071a9ed8c46b4fae51a253e28abc1
tanizawa_kenta <kt10025102@gmail.com>
2020-01-21 17:42:18 +0900

    [add]イベントビュー作成、イベント画像アップロード機能

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,49 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def edit
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to @event, notice: 'イベントを新規作成しました。'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to @event, notice: 'イベントを編集しました。'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_url, notice: 'イベントを削除しました。'
+  end
+
+  private
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    def event_params
+      params.require(:event).permit(:title, :place, :date, :content, :deadline, :capacity, :image, :image_cache)
+    end
 end
