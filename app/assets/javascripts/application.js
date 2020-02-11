b0e05d3b170197e9bad2df725525a229686deaf0
tanizawa_kenta <kt10025102@gmail.com>
2020-02-11 12:18:55 +0900

    [add]フォームの日時入力にてカレンダーでの入力機能を追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,8 @@
 //= require rails-ujs
 //= require activestorage
 //= require jquery/dist/jquery.js
+//= require moment
+//= require moment/ja.js
+//= require tempusdominus-bootstrap-4.js
 //= require turbolinks
 //= require_tree .
