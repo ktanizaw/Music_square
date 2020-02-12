795044b000c2d74c9550958be2def161afe20ad1
tanizawa_kenta <kt10025102@gmail.com>
2020-02-12 17:38:57 +0900

    [update]メッセージ修正

--- app/controllers/fans_controller.rb
+++ app/controllers/fans_controller.rb
@@ -1,11 +1,11 @@
 class FansController < ApplicationController
   def create
     fan = current_user.fans.create(artist_board_id: params[:artist_board_id])
-    redirect_to artist_board_path(fan.artist_board.artists), notice: "#{fan.artist_board.artists}のファンになりました！"
+    redirect_to artist_board_path(fan.artist_board.artists), notice: "#{fan.artist_board.artists}をファン登録しました！"
   end
 
   def destroy
     fan = current_user.fans.find_by(id: params[:id]).destroy
-    redirect_to artist_board_path(fan.artist_board.artists), notice: "#{fan.artist_board.artists}のファンを辞めました。"
+    redirect_to artist_board_path(fan.artist_board.artists), notice: "#{fan.artist_board.artists}のファン登録を解除しました。"
   end
 end
