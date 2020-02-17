76a0fcf342b2ce9c12912390f00812af3fe14fc5
tanizawa_kenta <kt10025102@gmail.com>
2020-02-17 16:31:43 +0900

    [update]root設定修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'users/registrations#new'
+  root 'artist_boards#index'
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, :controllers => {
