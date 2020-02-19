8109179a7342b0c961a703cae3cf46918b6a37e8
tanizawa_kenta <kt10025102@gmail.com>
2020-02-19 14:48:21 +0900

    [add]sendgrid設定追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: "heroku.com",
+    domain: "gmail.com",
     address: "smtp.SendGrid.net",
     port: 587,
     authentication: :plain,
