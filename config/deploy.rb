832bbcd6e5840524fdc5f614b4f9e9f609dd1c77
tanizawa_kenta <kt10025102@gmail.com>
2020-02-19 21:10:44 +0900

    [update]gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,9 +10,7 @@ set :deploy_to, '/var/www/Music_square'
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
-# 保持するバージョンの個数(※後述)
 set :keep_releases, 5
-# Rubyのバージョン
 set :rbenv_ruby, '2.6.5'
 set :rbenv_type, :system
 # 出力するログのレベル。エラーログを詳細に見たい場合は :debug に設定する。
