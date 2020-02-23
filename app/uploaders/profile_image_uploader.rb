4f6396afae709890cfc79260ebeec2d8f5d36f66
tanizawa_kenta <kt10025102@gmail.com>
2020-02-23 23:16:42 +0900

    [update]profile-imageアップローダー修正

--- app/uploaders/profile_image_uploader.rb
+++ app/uploaders/profile_image_uploader.rb
@@ -1,7 +1,7 @@
 class ProfileImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  storage :fog
+  storage :file
   process { :resize_to_limit [150, 150] }
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
