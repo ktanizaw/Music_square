c66be9f0a6e6284c6fecca3565dea40a10667c9a
tanizawa_kenta <kt10025102@gmail.com>
2020-02-24 17:13:21 +0900

    [update]コード規約に沿って修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,11 +5,11 @@ CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region:                'ap-northeast-1',
-      path_style:            true,
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1',
+      path_style: true,
   }
   config.fog_public     = true
   config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
